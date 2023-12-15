3b542e73f3a7cc81d101bd70d16ec9a567d3b330
ningbnii <>
2023-12-15 16:13:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import vant from 'vant'
+import { Button } from 'vant'
 import router from './router'
 import { createMetaManager } from 'vue-meta'
 import VuePageStack from 'vue-page-stack'
 import 'normalize.css'
+import 'vant/lib/index.css'
 
 const app = createApp(App)
-app.use(vant)
+app.use(Button)
 app.use(VuePageStack, { router })
 app.use(router)
 app.use(createMetaManager())
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,12 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [
     // 解释一下visualizer插件，这个插件可以将打包后的文件以可视化的方式展示出来，方便我们分析
     visualizer({ open: true }),
@@ -16,61 +22,56 @@ export default defineConfig({
     }),
     vue({
       // swiper-
-      template: {
-        compilerOptions: {
-          // 解释一下isCustomElement这个配置，这个配置可以让我们在vue文件中使用自定义标签，不会报错，比如swiper，这里我们配置了swiper开头的标签都是自定义标签，不会报错，这样我们就可以在vue文件中使用swiper标签了，而不需要在main.js中引入swiper
-          isCustomElement: (tag) => tag === 'swiper' || tag.startsWith('swiper-'),
-        },
-      },
+      // template: {
+      //   compilerOptions: {
+      //     // 解释一下isCustomElement这个配置，这个配置可以让我们在vue文件中使用自定义标签，不会报错，比如swiper，这里我们配置了swiper开头的标签都是自定义标签，不会报错，这样我们就可以在vue文件中使用swiper标签了，而不需要在main.js中引入swiper
+      //     isCustomElement: (tag) => tag === 'swiper' || tag.startsWith('swiper-'),
+      //   },
+      // },
     }),
-    importToCDN({
-      modules: [
-        {
-          name: 'vue',
-          var: 'Vue',
-          path: 'https://cdn.staticfile.org/vue/3.3.4/vue.global.prod.js',
-        },
-        {
-          name: 'vant',
-          var: 'vant',
-          path: 'https://cdn.staticfile.org/vant/4.6.8/vant.min.js',
-          css: 'https://cdn.staticfile.org/vant/4.6.8/index.min.css',
-        },
-        {
-          name: 'vue-router',
-          var: 'VueRouter',
-          path: 'https://cdn.staticfile.org/vue-router/4.2.4/vue-router.global.prod.js',
-        },
-        {
-          name: 'vue-meta',
-          var: 'VueMeta',
-          path: 'https://cdn.staticfile.org/vue-meta/3.0.0-alpha.10/vue-meta.global.min.js',
-        },
-        {
-          name: 'axios',
-          var: 'axios',
-          path: 'https://cdn.staticfile.org/axios/1.5.0/axios.min.js',
-        },
-        {
-          name: 'qs',
-          var: 'qs',
-          path: 'https://cdn.staticfile.org/qs/6.11.2/qs.min.js',
-        },
-      ],
-    }),
+    // importToCDN({
+    //   modules: [
+    //     {
+    //       name: 'vue',
+    //       var: 'Vue',
+    //       path: 'https://cdn.staticfile.org/vue/3.3.4/vue.global.prod.js',
+    //     },
+    //     {
+    //       name: 'vant',
+    //       var: 'vant',
+    //       path: 'https://cdn.staticfile.org/vant/4.6.8/vant.min.js',
+    //       css: 'https://cdn.staticfile.org/vant/4.6.8/index.min.css',
+    //     },
+    //     {
+    //       name: 'vue-router',
+    //       var: 'VueRouter',
+    //       path: 'https://cdn.staticfile.org/vue-router/4.2.4/vue-router.global.prod.js',
+    //     },
+    //     {
+    //       name: 'vue-meta',
+    //       var: 'VueMeta',
+    //       path: 'https://cdn.staticfile.org/vue-meta/3.0.0-alpha.10/vue-meta.global.min.js',
+    //     },
+    //     {
+    //       name: 'axios',
+    //       var: 'axios',
+    //       path: 'https://cdn.staticfile.org/axios/1.5.0/axios.min.js',
+    //     },
+    //     {
+    //       name: 'qs',
+    //       var: 'qs',
+    //       path: 'https://cdn.staticfile.org/qs/6.11.2/qs.min.js',
+    //     },
+    //   ],
+    // }),
   ],
   server: {
     open: true,
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     rollupOptions: {
       // 告诉打包工具，在external配置的包，都是外部引入的，不要打包到代码中
-      external: ['vue', 'vant', 'vue-router', 'vue-meta', 'axios', 'qs'],
+      // external: ['vue', 'vant', 'vue-router', 'vue-meta', 'axios', 'qs'],
       plugins: [
         // 解释一下externalGlobals插件，这个插件可以将external配置的包，以全局变量的方式引入，方便我们在代码中使用
         // externalGlobals({
@@ -97,6 +98,8 @@ export default defineConfig({
           if (id.includes('/node_modules/')) {
             // 让第三方模块单独打包
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
+            // 打包成一个
+            // return 'vendor'
           }
         },
         chunkFileNames: 'assets/js/[name]-[hash].js', // 代码分割后的文件名
